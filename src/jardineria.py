eb4ad5e1e9b66f3f5daabcf1dc13c3057812116e
cgaleam <cargalmag@alum.us.com>
2022-10-18 14:02:32 +0200

    Primera entrega

--- src/jardineria.py
+++ src/jardineria.py
@@ -1,23 +1,25 @@
 from collections import namedtuple
 import csv
-
 from convers import *
 
-Jardineria=namedtuple('Jardineria', 'calle, numero, fecha, hora, importe, num_jardineros, trabajos, contrato_mantenimiento')
+Jardineria=namedtuple('jardineria', 'calle, numero, fecha, hora, importe, num_jardineros, trabajos, contrato_mantenimiento')
 
 def lee_jardineria(nombre_fichero):
-    jardineria=[]
-    with open(nombre_fichero, encoding='utf-8') as f:
+    res=[]
+    with open(nombre_fichero,"rt", encoding='utf-8') as f:
         lector=csv.reader(f,delimiter=";")
         next(lector)
         for calle, numero, fecha, hora, importe, num_jardineros, trabajos, contrato_mantenimiento in lector:
             numero=int(numero)
             fecha=conv_date(fecha)
             hora=conv_hora(hora)
-            importe=float(importe)
+            importe=float(importe.replace(",","."))
             num_jardineros=int(num_jardineros)
-            contrato_mantenimiento=conv_bool(contrato_mantenimiento)
+            if contrato_mantenimiento=='S':
+                 contrato_mantenimiento=True
+            else:
+                contrato_mantenimiento=False
 
-            tupla=Jardineria(calle, numero, fecha, hora, importe, num_jardineros, trabajos, contrato_mantenimiento)
-            jardineria.append(tupla)
-    return jardineria
+            res.append(Jardineria(calle, numero, fecha, hora, importe, num_jardineros, trabajos, contrato_mantenimiento))
+            
+    return res
--- src/jardineria_test.py
+++ src/jardineria_test.py
@@ -1,8 +1,11 @@
 from jardineria import *
+def test_lee_jardineria ():
+    print("\nNúmero de registros leidos=", len(datos))
+    print("Los tres primeros registros son:", datos[:3])
+    print("Los tres ultimos registros son:", datos[:-3])
 
 
-def test_lee_jardineria():
-    print("el numero total de registros es:", len(datos))
 
-datos=lee_jardineria("./data/jardinería_50.csv")
-test_lee_jardineria
+#Principal
+datos=lee_jardineria("proyecto_primer_cuatrimestre-cgaleam/data/jardinería_50.csv")
+test_lee_jardineria()
